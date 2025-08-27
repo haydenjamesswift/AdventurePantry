828d9350e4955fd0c1edf743f101a298b913cd94
Hayden Swift <haydenjamesswift@gmail.com>
2025-08-27 12:44:02 +1000

    Final styles before continuing with coursework.

--- AP.WebApp/obj/Debug/net9.0/AP.WebApp.AssemblyInfo.cs
+++ AP.WebApp/obj/Debug/net9.0/AP.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AP.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb73fb21308720f4880bb578dbcb7e0febcc2cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f03527c628803ee7da73d79d83e2dc370aafb0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AP.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AP.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
